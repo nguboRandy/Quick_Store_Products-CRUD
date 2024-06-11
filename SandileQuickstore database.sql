fefbcd5f9f61adc567b73792d6998f3ef470fd24
nguboRandy <128244908+nguboRandy@users.noreply.github.com>
2024-06-11 22:26:30 +0200

    Add files via upload

---
+++ SandileQuickstore database.sql
@@ -0,0 +1,13 @@
+Create database SandileQuickstore;
+
+Use SandileQuickstore;
+
+create table PRODUCTS(
+Product_Name varchar(100),
+Quantity int,
+Price decimal(10,2) ,
+Stock_Value decimal(10,2));
+
+
+select * from PRODUCTS;
+
